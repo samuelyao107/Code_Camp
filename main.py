04e827499a1b3bd0241c493b1ff666cf84439cb8
SamuelFutur <Samuel.Yao@futurafric.com>
2024-09-09 17:37:42 +0200

    3rd commit

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_tasks(file_name):
+    if not os.path.exists(file_name):
+        return []
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    tasks = [line.strip().split(",", 1) for line in lines if line.strip()]
+    return [(int(task[0]), task[1]) for task in tasks]
 
+def write_tasks(file_name, tasks):
+    with open(file_name, 'w') as f:
+        for task in tasks:
+            f.write(f"{task[0]},{task[1]}\n")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def add_task(file_name, description):
+    tasks = read_tasks(file_name)
+    new_id = max([task[0] for task in tasks], default=0) + 1
+    tasks.append((new_id, description))
+    write_tasks(file_name, tasks)
+    print(f"Added task with ID: {new_id}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    parser = argparse.ArgumentParser(description='Simple Task Management System')
+    parser.add_argument('filename', help='Name of the file where tasks are stored')
+
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # Subparser for the "add" command
+    parser_add = subparsers.add_parser('add', help='Add a new task')
+    parser_add.add_argument('description', nargs='+', help='Description of the task')
+
+    args = parser.parse_args()
+
+    if args.command == 'add':
+        description = " ".join(args.description)
+        add_task(args.filename, description)
+
+if __name__ == "__main__":
+    main()
