6130b75219cac6c4e1053a755312f6953969a454
Nawal CHAHBOUNE <nawalchahbouneimt@gmail.com>
2024-09-09 18:06:34 +0200

    nawal

--- main.py
+++ main.py
@@ -7,10 +7,34 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def rm(fichier_name,id):
+    
+    lignes_mofifies=[]
+    
+    with open(fichier_name, 'r') as f:
+        lignes = f.readlines()
+        
+    for ligne in lignes:
+            #index+=1
+            partie_gauche, partie_droite = ligne.split(';', 1)
+            if partie_gauche!=id:
+                 lignes_mofifies.append(ligne)
+
+    with open(fichier_name,'w') as f:
+         f.writelines(lignes_mofifies)   
+            
+    print("ligne removed?")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #print_hi('PyCharm')
+    rm("taches.txt",4)
+    print("modified")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+       
+
+
