6dae664aa1b726aca82de770962a13b9b2747481
SamuelFutur <Samuel.Yao@futurafric.com>
2024-09-09 17:23:08 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import argparse
+import os
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# File structure: id,description
+def read_tasks(file_name):
+    if not os.path.exists(file_name):
+        return []
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    tasks = [line.strip().split(";", 1) for line in lines if line.strip()]
+    return [(int(task[0]), task[1]) for task in tasks]
 
+def write_tasks(file_name, tasks):
+    with open(file_name, 'w') as f:
+        for task in tasks:
+            f.write(f"{task[0]};{task[1]}\n")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def add_task(file_name, description):
+    tasks = read_tasks(file_name)
+    new_id = max([task[0] for task in tasks], default=0) + 1
+    tasks.append((new_id, description))
+    write_tasks(file_name, tasks)
+    print(f"Added task with ID: {new_id}")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
