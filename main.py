a53dd8611e853d0f94ad386947893aa2cc330831
Aeddan <aeddan.williams@imt-atlantique.net>
2024-09-09 17:03:37 +0200

    parser1

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
+parser = argparse.ArgumentParser
 
 
 def print_hi(name):
